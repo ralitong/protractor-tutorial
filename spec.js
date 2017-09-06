9b9ec44443fff7ca30da5ac8b00754e1a61ef6d5
ralitong <ral_itong@yahoo.com>
2017-09-06 21:14:05 +0800

    completed the tutorial module of protractor js site

--- spec.js
+++ spec.js
@@ -17,7 +17,6 @@ describe('Protractor Demo App', () => {
     });
 
     it('should add one and two', () => {
-        // browser.get('http://juliemr.github.io/protractor-demo/');
         firstNumber.sendKeys(1);
         secondNumber.sendKeys(2);
 
@@ -27,14 +26,37 @@ describe('Protractor Demo App', () => {
     });
 
     it('should add four and six', () => {
-        // browser.get('http://juliemr.github.io/protractor-demo/');
         firstNumber.sendKeys(4);
         secondNumber.sendKeys(6);
 
         goButton.click();
 
         expect(latestResult.getText()).toEqual('10');
-
     });
+
+    it('should have a history', ()=> {
+        add(1, 2);
+        add(3, 4);
+
+        expect(history.count()).toEqual(2);
+        
+        add(5, 6);
+
+        expect(history.count()).toEqual(3); 
+    });
+
+    it('history should contain the correct result', ()=>{
+        add(1, 2);
+        add(3, 4);
+    
+        expect(history.last().getText()).toContain('1 + 2');
+        expect(history.first().getText()).toContain('3 + 4'); // This is wrong!
+    })
+
+    function add(a, b) {
+        firstNumber.sendKeys(a);
+        secondNumber.sendKeys(b);
+        goButton.click();
+    }
 
 })
